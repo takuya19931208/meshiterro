a2e9402a603614a0e53b7be7407910302cf66212
takuya19931208 <takuya19931208@gmail.com>
2021-06-16 12:26:07 +0000

    [finish]6章

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,19 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id]) 
+    @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page]).reverse_order
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)  
+    redirect_to user_path(@user.id)
   end
-  
+
   private
 
   def user_params
