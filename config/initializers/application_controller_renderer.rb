8a4498b79da5125e97703c7ce2777efeb8c93e7d
takuya19931208 <takuya19931208@gmail.com>
2021-06-16 05:50:33 +0000

    [finish]3章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3b7e2beaba196aa44c4101fbd5c16cb0a6d9bb610881ff6c38d93446d64655c2e6faf9170b90f75c3157e9716c318afc775e7d5e5836421fa7fd36d755a4204b'
